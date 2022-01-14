f7aaf0872649f3524f32a967feaa8c9c4ea01008
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-01-14 20:28:10 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -3,7 +3,9 @@ class Presets(object):
 <b>Hello.. </b>{}
 <code>my self STUDENTA🇮🇳 bot brings latest educational updates of telegram to you
 maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ne updates .</code>
+<code>Keep me unblocked to receive ne updates  buttons = [
+        InlineKeyboardButton(
+            text="JOIN UPDATE CHANNEL", url="t.me/codesfru"), ] .</code>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
